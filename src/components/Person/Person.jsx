982164757c7236734ba4d359b2149e3f7d4966b7
Julio Vergueiro <89145136+Juliovergueiro@users.noreply.github.com>
2025-07-30 22:03:58 -0300

    First Commit

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,12 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => (
+  <section className="Person">
+    <h2 className="Person__name">My name is {person.name}</h2>
+    <p className="Person__age">
+      I am {person.age && <span>{person.age}</span>}
+    </p>
+    <p className="Person__partner">
+      {person.isMarried && <span>{person.partnerName} is my wife</span>}{' '}
+      {!person.isMarried && <span>I am not married</span>}
+    </p>
+  </section>
+);
