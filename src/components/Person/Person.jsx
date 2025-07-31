605010b91db4ffb8161743e4e3cc12def23dbee1
Julio Vergueiro <89145136+Juliovergueiro@users.noreply.github.com>
2025-07-30 22:25:28 -0300

    second Commit

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -5,7 +5,12 @@ export const Person = ({ person }) => (
       I am {person.age && <span>{person.age}</span>}
     </p>
     <p className="Person__partner">
-      {person.isMarried && <span>{person.partnerName} is my wife</span>}{' '}
+      {person.isMarried && (
+        <span>
+          {person.partnerName} is my {person.sex === 'm' && 'wife'}{' '}
+          {person.sex === 'f' && 'husband'}
+        </span>
+      )}{' '}
       {!person.isMarried && <span>I am not married</span>}
     </p>
   </section>
